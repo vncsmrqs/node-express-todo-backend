e3f194ef6fc2027672ea5c22277fa724250a7af3
vncsmrqs <vncsmrqs@gmail.com>
2018-12-12 11:53:03 -0300

    Atualização do cabeçalho do CORS

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,9 +1,9 @@
-//Em um middle ou eu respondo a requisição ou eu chamo o NEXT
+﻿//Em um middle ou eu respondo a requisição ou eu chamo o NEXT
 
 module.exports = function(req, res, next) { //Isso é um MIDDLEWARE
     res.header('Access-Control-Allow-Origin', '*') //Passo os endereçes dos clientes que podem consumir a API
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE') //Metodos do HTTP aceitos
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-type')
     next() //Continua o fluxo da aplicação. Sem ele a aplicação vai parar aqui
 }
 //Estou trabalhando com a resposta
